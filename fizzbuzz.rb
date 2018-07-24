887e583a94c354ab2a4754a377fefa1c87e273f7
madddanz <maddiedanzberger@gmail.com>
2018-07-24 18:30:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,6 +3,7 @@ if fizzbuzz%3 == 0
   puts "fizz"
 elsif fizzbuzz%5 == 0
  puts "buzz"
- elsif fizzbuzz%3 == 0 && fizzbuzz%5 == 0
+elsif fizzbuzz%3 == 0 && fizzbuzz%5 == 0
 puts "fizzbuzz"
 else NIL
+end
