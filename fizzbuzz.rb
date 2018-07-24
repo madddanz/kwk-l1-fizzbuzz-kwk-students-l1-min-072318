4587bb04e5127eb1801859d1b924971385d5beb6
madddanz <maddiedanzberger@gmail.com>
2018-07-24 18:39:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,12 @@
 def fizzbuzz(number)
 if number%3 == 0 && number%5 != 0 
-  puts "Fizz"
+ "Fizz"
 elsif number%5 == 0 && number%3 != 0
- puts "Buzz"
+ "Buzz"
 elsif number%3 == 0 && number%5 == 0
-puts "FizzBuzz"
-else NIL
+  "FizzBuzz"
 end
 end
 
 
-fizzbuzz(25)
+puts fizzbuzz(25)
