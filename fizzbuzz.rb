2f10d4acd6e2a15d48e5abafdb6f0f9a27fc4001
madddanz <maddiedanzberger@gmail.com>
2018-07-24 18:34:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
 if number%3 == 0 && number%5 != 0 
-  puts "fizz"
+  puts "Fizz"
 elsif number%5 == 0 && number%3 != 0
- puts "buzz"
+ puts "Buzz"
 elsif number%3 == 0 && number%5 == 0
-puts "fizzbuzz"
+puts "FizzBuzz"
 else 
   puts "that doesn't work!!"
 end
